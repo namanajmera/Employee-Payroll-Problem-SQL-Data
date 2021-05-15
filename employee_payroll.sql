620ab35efe1c322107eb3e10401e67a9f23910fd
Naman <cool.naman.ajmera@gmail.com>
2021-05-15 12:30:58 +0530

    Added: Adding the columna dn update the row values.

--- employee_payroll.sql
+++ employee_payroll.sql
@@ -31,3 +31,8 @@ select salary from employee_payroll where name='Bill';
 
 select * from employee_payroll 
 where start between cast('2018-01-01' as date) and date(now());
+
+#UC6
+alter table employee_payroll add gender char(1) after name;
+
+update employee_payroll set gender='M' where name='Bill' or name='Charlie';
