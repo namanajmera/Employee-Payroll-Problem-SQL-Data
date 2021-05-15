8fc3a41e77adf1bfdbcae95b0cb8f5654d1e245c
Naman <cool.naman.ajmera@gmail.com>
2021-05-15 11:52:30 +0530

    Added; Create a table name employee_payroll.

--- employee_payroll.sql
+++ employee_payroll.sql
@@ -4,3 +4,14 @@ show databases;
 create database payroll_service_db;
 
 use payroll_service_db;
+
+#UC2
+create table employee_payroll
+(
+	id int unsigned NOT NULL AUTO_INCREMENT,
+    name varchar(150) NOT NULL,
+    salary double NOT NULL,
+    start date NOT NULL,
+    PRIMARY KEY (id)
+);
+show tables;
